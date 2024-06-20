a3a1060466fd83c49b550e4f58b5b87180d49d4c
Carolina Bispo <carolinabispos07@gmail.com>
2024-06-20 16:02:31 -0300

    feito o menu

---
+++ bank_system.py
@@ -0,0 +1,16 @@
+menu = """
+=========== BEM VINDO AO DIO-bank ============
+
+    Selecione qual a opção deseja abaixo:
+
+    [1] Depósitos
+    [2] Saques
+    [3] Extratos
+    [4] Sair
+"""
+saldo = 0
+SAQUES_DIARIOS = 3
+limite = 500
+numero_saques = 0
+extrato = ""
+
